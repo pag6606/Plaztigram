504828917412e190ff0d4091606c38890a8f7336
Paul <paul.alarcon@quantum.ec>
2016-11-06 11:39:46 -0500

    Initial commit

---
+++ app/src/main/java/com/consultoresag/plaztigram/view/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.consultoresag.plaztigram.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.consultoresag.plaztigram.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
