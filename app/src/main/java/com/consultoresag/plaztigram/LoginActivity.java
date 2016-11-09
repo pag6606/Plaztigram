e4a5e1e549a8e5b9ff753439ceb1a7a813d57b9d
Paul <paul.alarcon@quantum.ec>
2016-11-08 20:40:24 -0500

    se creo la interfaz de crear cuenta

--- app/src/main/java/com/consultoresag/plaztigram/LoginActivity.java
+++ app/src/main/java/com/consultoresag/plaztigram/LoginActivity.java
@@ -2,12 +2,24 @@ package com.consultoresag.plaztigram;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView registro = (TextView)findViewById(R.id.create_here);
+        registro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
--- app/src/main/java/com/consultoresag/plaztigram/view/CreateAccountActivity.java
+++ app/src/main/java/com/consultoresag/plaztigram/view/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package com.consultoresag.plaztigram.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.consultoresag.plaztigram.R;
 
@@ -11,5 +12,14 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        //se declara el toolbar
+        showToolbar(getResources().getString(R.string.toolbar_title_create_account),false);
+
+    }
+    public void showToolbar(String title, boolean upButton){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 }
