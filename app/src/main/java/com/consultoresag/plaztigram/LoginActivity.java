67eecc821a47a3b07846ac2e97b6cdef87306a79
Paul <paul.alarcon@quantum.ec>
2016-11-20 21:47:57 -0500

    se crea el cardview

--- app/src/main/java/com/consultoresag/plaztigram/LoginActivity.java
+++ app/src/main/java/com/consultoresag/plaztigram/LoginActivity.java
@@ -4,9 +4,13 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import com.consultoresag.plaztigram.view.ContainerActivity;
 import com.consultoresag.plaztigram.view.CreateAccountActivity;
+
+import static android.R.attr.button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -21,6 +25,15 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
             Intent intent =  new Intent(LoginActivity.this, CreateAccountActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button botonLogin = (Button) findViewById(R.id.login);
+        botonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent =  new Intent(LoginActivity.this, ContainerActivity.class);
                 startActivity(intent);
             }
         });
